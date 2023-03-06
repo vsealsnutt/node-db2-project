4b16a7142cccf3f849acde2ed60619d660e11e33
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-06 15:26:05 -0500

    wrote the up and down functions in the migration file using the given schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('id')
+    table.string('vin').notNullable().unique()
+    table.string('make').notNullable()
+    table.string('model').notNullable()
+    table.integer('mileage').notNullable()
+    table.string('title')
+    table.string('transmission')
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
