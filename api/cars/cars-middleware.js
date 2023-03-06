c1be4f357a3698cb16a2988c2f8a41ee73a067c3
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-06 15:42:21 -0500

    wrote checkCarId middleware function

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,34 @@
-const checkCarId = (req, res, next) => {
+const Car = require('./cars-model');
+
+async function checkCarId(req, res, next) {
+  try {
+    const car = await Car.getById(req.params.id);
+    if (!car) {
+      next({ status: 404, message: `car with id ${req.params.id} is not found` })
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+function checkCarPayload(req, res, next) {
   // DO YOUR MAGIC
 }
 
-const checkCarPayload = (req, res, next) => {
+function checkVinNumberValid(req, res, next) {
   // DO YOUR MAGIC
 }
 
-const checkVinNumberValid = (req, res, next) => {
+async function checkVinNumberUnique(req, res, next) {
   // DO YOUR MAGIC
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
