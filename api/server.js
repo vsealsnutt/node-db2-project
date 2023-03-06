627aa08ed088daae5583611333470078a4d05615
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-06 16:00:51 -0500

    wrote error handling middleware in server.js

--- api/server.js
+++ api/server.js
@@ -6,4 +6,10 @@ const server = express();
 server.use(express.json());
 server.use('/api/cars', carsRouter);
 
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+});
+
 module.exports = server
